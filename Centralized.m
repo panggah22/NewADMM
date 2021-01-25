d4173c611faf7ee04b17960bbe30137ab777e743
panggah22 <panggah22@gmail.com>
2021-01-25 14:20:38 +0900

    Added Constraints 3-5, Objective function, and MIQP call

--- Centralized.m
+++ Centralized.m
@@ -1,4 +1,32 @@
 %% Implementation part-by-part
+clear;
+
+% Parameters
+ts = 24;
+unc_mean = 0;
+unc_stdv = 1;
+unc_val = 0.99;
+maxiter = 500;
+rho = 10;
+isl = 1:33;
+fic = 1;
+
+data = data33(ts,unc_val,isl,fic);
+evr = length(data.k2);
+[len, lent] = lengthvar(data,evr,ts);
+inp = inputvar(lent);
+
+%% Objective Function : r * (Pline^2 + Qline^2)
+%% Execute MIQP
+H1 = zeros(lent.total);
+H1(inp.Pline,inp.Pline) = diagrep(eye(len.Pline).*data.branch(:,6)*2,ts);
+H1(inp.Qline,inp.Qline) = diagrep(eye(len.Qline).*data.branch(:,6)*2,ts);
+
+H2 = zeros(lent.total);
+
+H = H1+(rho/2*H2);
+
+f = zeros(lent.total,1);
 
 %% Constraint 1. Active Power Balance
 % Searching for bus connection
@@ -51,3 +79,83 @@ craqq = data.bus(:,4);
 for ii = 1:ts
     equ(11).beq((ii-1)*data.num_bus+(1:data.num_bus)) = craqq .* data.loadcoeff(ii);
 end
+
+%% Constraint 3. Voltage Drop
+V12 = zeros(data.num_branch,data.num_bus);
+for ii = 1:data.num_branch
+    V12(ii,data.branch(ii,2)) = -1;
+    V12(ii,data.branch(ii,3)) = 1;
+end
+Pline12 = eye(len.Pline) .* data.branch(:,6);
+Qline12 = eye(len.Qline) .* data.branch(:,7);
+
+equ(12).Aeq = zeros(lent.Pline,lent.total);
+equ(12).Aeq(:,[inp.V, inp.Pline, inp.Qline]) = [diagrep(V12,ts), diagrep(Pline12,ts), diagrep(Qline12,ts)];
+equ(12).beq = zeros(size(equ(12).Aeq,1),1);
+
+%% Constraint 4. Voltage Limit
+% A
+V13a = -eye(len.V);
+b13a = -ones(len.V,1) .* 0.9;
+
+% B
+V13b = eye(len.V);
+b13b = ones(len.V,1) .* 1.1;
+
+ineq(13).A = zeros(lent.V*2,lent.total);
+ineq(13).A(:,inp.V) = [diagrep(V13a,ts); diagrep(V13b,ts)];
+ineq(13).b = [repmat(b13a,ts,1); repmat(b13b,ts,1)];
+
+%% Constraint 5. Tap Changer
+V14 = zeros(1,len.V);
+V14(1) = 1;
+Tap14 = -eye(size(V14,1)) .* 0.00625;
+
+equ(14).Aeq = zeros(ts,lent.total);
+equ(14).Aeq(:,inp.V) = diagrep(V14,ts);
+equ(14).Aeq(:,inp.Tap) = diagrep(Tap14,ts);
+equ(14).beq = ones(size(equ(14).Aeq,1),1);
+
+%% Concatenate constraints
+Cequ = cell(size(equ,2),1);
+Cbequ = Cequ;
+for i = 1:size(equ,2)
+    Cequ{i} = equ(i).Aeq;
+    Cbequ{i} = equ(i).beq;
+end
+Aeq = cell2mat(Cequ);
+beq = cell2mat(Cbequ);
+
+Cineq = cell(size(ineq,2),1);
+Cbineq = Cineq;
+for i = 1:size(ineq,2)
+    Cineq{i} = ineq(i).A;
+    Cbineq{i} = ineq(i).b;
+end
+Aineq = cell2mat(Cineq);
+bineq = cell2mat(Cbineq);
+
+%% Bounds
+lb = -inf(lent.total,1);
+lb(inp.PEV) = 0;
+lb(inp.bin) = 0;
+lb(inp.Tap) = -16;
+
+ub = inf(lent.total,1);
+ub(inp.bin) = 1;
+ub(inp.Tap) = 16;
+ub([inp.cmin inp.cmax]) = 20;
+
+%% Continuous and integer
+ctypenum = 67*ones(1,lent.total);
+ctypenum(inp.bin) = 73;
+
+ctype = char(ctypenum);
+
+options = cplexoptimset('cplex');
+options.display = 'on';
+% options.mip.tolerances.mipgap = 1/100;
+options.timelimit = 600;
+%% Execute MIQP
+[xx, fval, exitflag, output] = cplexmiqp (H, f, Aineq, bineq, Aeq, beq,...
+    [], [], [], lb, ub, ctype, [], options);
